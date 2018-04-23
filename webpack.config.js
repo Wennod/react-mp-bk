8e48b9a1408b3cd7718d5261b6ec8f5c48142c02
Bahdan Kutas <bahdan_kutas@epam.com>
2018-04-23 14:05:35 +0300

    process-commit

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports =  function(env, options) {
+	const isProduction = options.mode === 'production';
+
+	const isOptimized = {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					name: 'vendor',
+					chunks: 'all',
+					minChunks: 2
+				}
+			}
+		}
+	}
+
+
+	const config = {
+		entry: {
+			app: './index.js',
+			vendor: [
+				'react',
+				'react-dom',
+				'react-redux',
+				'redux'
+			]
+		},
+
+		mode: isProduction ? 'production' : 'development',
+		devtool: isProduction ? 'none' : 'source-map',
+		optimization: isOptimized,
+
+		resolve: {
+		  extensions: ['.js', '.jsx']
+		},
+
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['env', 'react'],
+							plugins: ['syntax-object-rest-spread'],
+						}
+					}
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader']
+				}
+			]
+		},
+
+		output: {
+			filename: '[name].bundle.js',
+			path: path.resolve(__dirname, 'dist')
+		},
+
+		plugins: [
+			new HtmlWebpackPlugin({
+				hash: true,
+				myPageHeader: 'rmp',
+				template: './index.html',
+				path: path.join(__dirname, 'dist'),
+				filename: 'index.html'
+			})	
+		],
+
+		devServer: {
+			contentBase: path.join(__dirname, 'dist'),
+			port: 5500,
+		}
+	}
+
+	return config;
+};
