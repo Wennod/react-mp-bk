e827f2c6d9d3243befc1335f6f929b6e752aee73
Bahdan.Kutas <bahdan_kutas@epam.com>
2018-05-21 17:42:45 +0300

    feature/e2e left

--- src/components/QueryPage/ResultsContainer/ResultsContainer.spec.js
+++ src/components/QueryPage/ResultsContainer/ResultsContainer.spec.js
@@ -2,11 +2,39 @@ import React from 'react';
 import ResultsContainer from './ResultsContainer';
 import renderer from 'react-test-renderer';
 
+const __data = [
+    {
+        "id": 412302,
+        "title": "Gemini",
+        "tagline": "",
+        "vote_average": 10,
+        "vote_count": 3,
+        "release_date": "2018-03-30",
+        "poster_path": "https: //image.tmdb.org/t/p/w500/oIltQs7MPk7VQFG3DJfgC63mShU.jpg",
+        "budget": 0,
+        "revenue": 0,
+        "genres":  ["Mystery","Thriller"],
+        "runtime": 92
+    }
+]
+
 describe('ResultsContainer snapshot', () => {
     it('should match snapshot', () => {
         const tree = renderer
             .create(<ResultsContainer />)
             .toJSON();
         expect(tree).toMatchSnapshot();
+    });
+});
+
+describe('ResultsContainer', () => {
+    it('should receive props', () => {
+        const tree = mount(<ResultsContainer data={__data} />);
+        expect(tree.props().data && tree.props().data.length).toBeTruthy();
+    });
+
+    it('should behave properly in case if no data received', () => {
+        const tree = mount(<ResultsContainer data={__data} />);
+        expect(tree.props().data && tree.props().data.length).toBeFalsy;
     });
 });
