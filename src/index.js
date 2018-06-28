f88a8079b13a028549d717e54189d1f39043304f
Bahdan.Kutas <bahdan_kutas@epam.com>
2018-06-28 09:54:52 +0300

    redux-persist added to app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
-import store from './reducers/store';
+import { store, persistor } from './reducers/store';
 import indexStyle from './index.scss';
 
 import App from './App';
 
 render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,    
     document.getElementById('root')
 );
--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,10 +1,20 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 import search from './search.reducer';
 
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
 const rootReducer = combineReducers({ search });
 
-export default createStore(rootReducer, applyMiddleware(thunk, logger));
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk, logger));
+export const persistor = persistStore(store);
